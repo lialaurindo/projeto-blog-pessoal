2ca83b0c9d3e856e7baea4d4d78d22ed7993c663
lialaurindo <liamaralaurindo@gmail.com>
2023-03-24 14:22:23 -0300

    cria estrutura de classes e repository para Usuario

---
+++ blogpessoal/src/main/java/com/generation/blogpessoal/model/Usuario.java
@@ -0,0 +1,93 @@
+package com.generation.blogpessoal.model;
+
+import java.util.List;
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+
+@Entity
+@Table(name = "tb_usuarios")
+public class Usuario {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@NotBlank(message = "O atributo nome é obrigatório")
+	private String nome;
+	
+	@NotBlank(message = "O atributo usuario é obrigatório")
+	@Email(message = "O atributo usuário deve ser um email válido")
+	private String usuario;
+	
+	@NotBlank(message = "O atributo senha é obrigatório")
+	@Size(min = 8, message = "A senha deve ter mínimo 8 caracteres ")
+	private String senha;
+	
+	@NotBlank(message = "O atributo foto é obrigatório")
+	@Size(max = 5000, message = "O link da foto não pode ter mais de 5000 caracteres")
+	private String foto;
+	
+	@OneToMany(mappedBy = "usuario", cascade = CascadeType.REMOVE)
+	@JsonIgnoreProperties("usuario")
+	private List<Postagem> postagem;
+	
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getUsuario() {
+		return usuario;
+	}
+
+	public void setUsuario(String usuario) {
+		this.usuario = usuario;
+	}
+
+	public String getSenha() {
+		return senha;
+	}
+
+	public void setSenha(String senha) {
+		this.senha = senha;
+	}
+
+	public String getFoto() {
+		return foto;
+	}
+
+	public void setFoto(String foto) {
+		this.foto = foto;
+	}
+
+	public List<Postagem> getPostagem() {
+		return postagem;
+	}
+
+	public void setPostagem(List<Postagem> postagem) {
+		this.postagem = postagem;
+	}
+	
+}
