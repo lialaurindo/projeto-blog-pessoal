1a773495af8977939f38ce8586e0852ed619a92c
lialaurindo <liamaralaurindo@gmail.com>
2023-03-24 14:28:29 -0300

    cria relacionamento @ManyToOne com Usuario

--- blogpessoal/src/main/java/com/generation/blogpessoal/model/Postagem.java
+++ blogpessoal/src/main/java/com/generation/blogpessoal/model/Postagem.java
@@ -38,6 +38,10 @@ public class Postagem {
 	@ManyToOne
 	@JsonIgnoreProperties("postagem")
 	private Tema tema;
+	
+	@ManyToOne
+	@JsonIgnoreProperties("postagem")
+	private Usuario usuario;
 
 	public Long getId() {
 		return id;
@@ -79,6 +83,12 @@ public class Postagem {
 		this.tema = tema;
 	}
 
+	public Usuario getUsuario() {
+		return usuario;
+	}
 
+	public void setUsuario(Usuario usuario) {
+		this.usuario = usuario;
+	}
 	
 }
